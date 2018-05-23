c800c9c7c22019d730e2aa4c079503fa20fea06f
root <root@ip-172-31-29-195.localdomain>
2018-05-24 09:07:31 +1000

    aws_opsworks_instance

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,7 +13,7 @@ template '/var/www/html/index.html' do
   owner 'root'
   variables(
    :motd => "this is the message mate!",
-   :hostname => "#{node['aws_opsworks_agent']['instance']['hostname']}"
+   :hostname => "#{node['aws_opsworks_instance']['hostname']}"
 
 )
 end
